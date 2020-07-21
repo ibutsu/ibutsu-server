f0c8e9eb3e540b62be6826f774237cace5f9b7b3
john-dupuy <jdupuy@redhat.com>
2020-07-21 14:37:35 -0400

    Version 1.10.0

--- backend/setup.py
+++ backend/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 NAME = "ibutsu_server"
-VERSION = "1.9.0"
+VERSION = "1.10.0"
 REQUIRES = [
     # Pin Celery to be compatible with Kombu
     "celery==4.3.0",
