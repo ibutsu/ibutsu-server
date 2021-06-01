9f0b43ada602f25803e3e1019ca4c2da9292b2aa
john-dupuy <jdupuy@redhat.com>
2021-06-01 14:53:34 -0400

    Small bugs fixes

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 import os
 
-workers = int(os.environ.get("GUNICORN_PROCESSES", "3"))
+workers = int(os.environ.get("GUNICORN_PROCESSES", "1"))
 threads = int(os.environ.get("GUNICORN_THREADS", "1"))
 
 forwarded_allow_ips = "*"
