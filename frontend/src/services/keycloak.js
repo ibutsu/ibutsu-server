d7fadc6ee11678bde17fe7ea8b2c13ef99e767f6
Raoul Snyman <rsnyman@redhat.com>
2021-12-15 16:00:19 -0700

    Fix keycloak login

--- frontend/src/services/keycloak.js
+++ frontend/src/services/keycloak.js
@@ -4,8 +4,8 @@ import { Settings } from '../settings';
 export class KeycloakService {
 
   static login(url, realm, client_id) {
-    const keycloakInstance = new Keycloak({url: url, realm: realm, clientId: client_id,
-      responseMode: 'query', redirectUri: Settings.serverUrl + '/login/auth/keycloak'});
-    keycloakInstance.init({onLoad: 'login-required', checkLoginIframe: false});
+    const keycloakInstance = new Keycloak({url: url, realm: realm, clientId: client_id});
+    keycloakInstance.init({onLoad: 'login-required', checkLoginIframe: false, responseMode: 'query',
+                           redirectUri: Settings.serverUrl + '/login/auth/keycloak'});
   }
 }
