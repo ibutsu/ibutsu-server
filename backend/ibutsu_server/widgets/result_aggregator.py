4b756a38093c7df4b0d19e9136ea049ff47050e5
eshamard <eshamard@redhat.com>
2024-04-08 18:01:38 +0200

    Correct check condition on group field

--- backend/ibutsu_server/widgets/result_aggregator.py
+++ backend/ibutsu_server/widgets/result_aggregator.py
@@ -36,7 +36,7 @@ def _get_recent_result_data(group_field, days, project=None, run_id=None, additi
 
     # generate the group field
     group_field = string_to_column(group_field, Result)
-    if not group_field:
+    if group_field is None:
         return []
 
     # create the query
