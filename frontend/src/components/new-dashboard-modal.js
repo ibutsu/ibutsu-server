e1357fa69f282d49ce31291b16d6e060dfae42f3
Mike Shriver <mshriver@redhat.com>
2025-02-11 10:50:44 -0500

    Convert NewDashboardModal to functional (#552)

--- frontend/src/components/new-dashboard-modal.js
+++ frontend/src/components/new-dashboard-modal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import {
@@ -11,83 +11,88 @@ import {
 } from '@patternfly/react-core';
 import { ExclamationCircleIcon } from '@patternfly/react-icons';
 
+const NewDashboardModal = (props) => {
+  const {
+    project,
+    saveCallback,
+    closeCallback,
+    isOpen
+  } = props;
 
-export class NewDashboardModal extends React.Component {
-  static propTypes = {
-    project: PropTypes.object,
-    onSave: PropTypes.func,
-    onClose: PropTypes.func,
-    isOpen: PropTypes.bool
-  };
+  const [title, setTitle] = useState('');
+  const [description, setDescription ] = useState('');
+  const [isTitleValid, setIsTitleValid ] = useState(false);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      widgetTypes: [],
-      title: '',
-      description: '',
-      isTitleValid: false
-    };
-  }
-
-  onTitleChange = (title) => {
-    this.setState({title});
-  }
-
-  onDescriptionChange = (description) => {
-    this.setState({description});
-  }
-
-  onSave = () => {
-    if (this.state.title === '') {
-      this.setState({isTitleValid: false});
+  const modalOnSave = () => {
+    console.dir(project);
+    if (title === '') {
+      setIsTitleValid(false);
       return;
     }
     let newDashboard = {
-      title: this.state.title,
-      description: this.state.description
+      title: title,
+      description: description,
+      project_id: project?.id
     };
-    if (this.props.project) {
-      newDashboard.project_id = this.props.project.id;
-    }
-    this.props.onSave(newDashboard);
-    this.setState({
-      title: '',
-      description: '',
-      isTitleValid: false
-    });
+    saveCallback(newDashboard);
+    setTitle();
+    setDescription();
+    setIsTitleValid(false);
   }
 
-  onClose = () => {
-    this.setState({
-      title: '',
-      description: '',
-      isTitleValid: false
-    });
-    this.props.onClose();
+  const modalOnClose = () => {
+    closeCallback();
+    setTitle();
+    setDescription();
+    setIsTitleValid(false);
   }
 
-  render () {
-    return (
-      <Modal
-        variant={ModalVariant.small}
-        title="New Dashboard"
-        isOpen={this.props.isOpen}
-        onClose={this.onClose}
-        actions={[
-          <Button key="save" variant="primary" onClick={this.onSave}>Save</Button>,
-          <Button key="cancel" variant="link" onClick={this.onClose}>Cancel</Button>
-        ]}
-      >
-        <Form>
-          <FormGroup label="Title" fieldId="dashboard-title" helpertextinvalid="A dashboard title is required" helpertextinvalidicon={<ExclamationCircleIcon />} validated={this.state.isTitleValid.toString()} isRequired>
-            <TextInput type="text" id="dashboard-title" name="dashboard-title" value={this.state.title} onChange={(_event, title) => this.onTitleChange(title)} validated={this.state.isTitleValid} isRequired />
-          </FormGroup>
-          <FormGroup label="Description" fieldId="dashboard-description">
-            <TextInput type="text" id="dashboard-description" name="dashboard-description" value={this.state.description} onChange={(_event, description) => this.onDescriptionChange(description)} />
-          </FormGroup>
-        </Form>
-      </Modal>
-    );
-  }
+  return(
+    <Modal
+      variant={ModalVariant.small}
+      title="New Dashboard"
+      isOpen={isOpen}
+      onClose={closeCallback}
+      actions={[
+        <Button key="save" variant="primary" onClick={modalOnSave}>Save</Button>,
+        <Button key="cancel" variant="link" onClick={modalOnClose}>Cancel</Button>
+      ]}
+    >
+      <Form>
+        <FormGroup
+          label="Title"
+          fieldId="dashboard-title"
+          helpertextinvalid="A dashboard title is required"
+          helpertextinvalidicon={<ExclamationCircleIcon />}
+          validated={isTitleValid.toString()}
+          isRequired>
+            <TextInput
+              type="text"
+              id="dashboard-title"
+              name="dashboard-title"
+              value={title}
+              onChange={(_event, value) => setTitle(value)}
+              validated={isTitleValid}
+              isRequired />
+        </FormGroup>
+        <FormGroup label="Description" fieldId="dashboard-description">
+          <TextInput
+            type="text"
+            id="dashboard-description"
+            name="dashboard-description"
+            value={description}
+            onChange={(_event, value) => setDescription(value)} />
+        </FormGroup>
+      </Form>
+    </Modal>
+  )
 }
+
+NewDashboardModal.propTypes = {
+  project: PropTypes.object,
+  saveCallback: PropTypes.func,
+  closeCallback: PropTypes.func,
+  isOpen: PropTypes.bool
+}
+
+export default NewDashboardModal
