53ffba374773a5d38db44aa65a8006add40d49b6
mshriver <mshriver@redhat.com>
2025-02-18 10:05:59 -0500

    Convert UserProfile

--- frontend/src/pages/profile/user.js
+++ frontend/src/pages/profile/user.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import {
   Alert,
   Button,
@@ -25,193 +24,157 @@ import { getDarkTheme } from '../../utilities';
 import ToastWrapper from '../../components/toast-wrapper';
 
 
-export class UserProfile extends React.Component {
-  static propTypes = {
-    eventEmitter: PropTypes.object
-  };
+const UserProfile = () => {
+  const [user, setUser] = useState();
+  const [projects, setProjects] = useState();
+  const [isEditing, setIsEditing] = useState();
+  const [tempName, setTempName] = useState('');
 
-  constructor (props) {
-    super(props);
-    this.eventEmitter = props.eventEmitter;
-    this.state = {
-      user: null,
-      projects: null,
-      isEditing: false
-    };
-  }
-
-  updateUserName (userName) {
-    if (!this.eventEmitter) {
-      return;
-    }
-    this.eventEmitter.emit('updateUserName', userName);
-  }
-
-  getUser () {
+  useEffect(() => {
+    // get user
     HttpClient.get([Settings.serverUrl, 'user'])
-      .then(response => {
-        response = HttpClient.handleResponse(response, 'response');
-        return response.json();
-      })
-      .then(user => this.setState({user}))
-      .catch(error => console.error(error));
-  }
-
-  getProjects () {
-    HttpClient.get([Settings.serverUrl, 'project'])
-      .then(response => HttpClient.handleResponse(response, 'response').json())
-      .then(data => this.setState({projects: data.projects}))
-      .catch(error => console.error(error));
-  }
-
-  saveUser (user) {
-    return HttpClient.put([Settings.serverUrl, 'user'], {}, user)
       .then(response => HttpClient.handleResponse(response))
+      .then(data => setUser(data))
       .catch(error => console.error(error));
-  }
+  }, []);
 
-  onEditButtonClicked = () => {
-    this.setState({tempName: this.state.user.name, isEditing: true});
-  };
+  useEffect(() => {
+    // get projects
+    HttpClient.get([Settings.serverUrl, 'project'])
+      .then(response => HttpClient.handleResponse(response))
+      .then(data => setProjects(data.projects))
+      .catch(error => console.error(error));
+  }, []);
 
-  onCancelButtonClicked = () => {
-    this.setState({isEditing: false});
-  };
-
-  onSaveButtonClicked = () => {
-    const { user, tempName } = this.state;
-    let tempUser = Object.assign({}, user, {name: tempName});
-    this.saveUser(tempUser).then(response => {
-      if (response !== undefined) {
-        toast(<ToastWrapper/>, {
-          data: {
-            type: 'success',
-            title: 'Name Updated',
-            message: 'Your name has been updated.'
-          },
-          type: 'success',
-          theme: getDarkTheme() ? 'dark' : 'light'
-        });
-        this.setState({user: tempUser, isEditing: false});
-        this.updateUserName(tempName);
-      }
-      else {
-        toast(ToastWrapper,
-          {
+  const onSaveButtonClicked = () => {
+    let tempUser = {...user, 'name': tempName};
+    HttpClient.put([Settings.serverUrl, 'user'], {}, user)
+      .then(response => HttpClient.handleResponse(response))
+      .then(data => {
+        if (data) {
+          toast(<ToastWrapper/>, {
             data: {
-              type: 'danger',
-              title: 'Error Updating',
-              message: 'Your name has NOT been updated.'
+              type: 'success',
+              title: 'Name Updated',
+              message: 'Your name has been updated.'
             },
-            type: 'danger',
+            type: 'success',
             theme: getDarkTheme() ? 'dark' : 'light'
           });
-        this.setState({isEditing: false});
-      }
-    });
+          setUser(tempUser);
+          setIsEditing(false);
+        } else {
+          toast(ToastWrapper,
+            {
+              data: {
+                type: 'danger',
+                title: 'Error Updating',
+                message: 'Your name has NOT been updated.'
+              },
+              type: 'danger',
+              theme: getDarkTheme() ? 'dark' : 'light'
+            });
+          setIsEditing(false);
+        }
+      })
+      .catch(error => console.error(error));
   };
 
-  componentDidMount () {
-    this.getUser();
-    this.getProjects();
-  }
+  document.title = 'Profile | Ibutsu';
 
-  render () {
-    document.title = 'Profile | Ibutsu';
-
-    const { user, projects } = this.state;
-    let projectInfo = [];
-    // create the project rows
-    if (projects && user) {
-      projectInfo.push(projects.map((project) => (
-        <DataListCell key={project.name} className="pf-u-p-sm">
-          <span> {project.title} </span>
-          {project.owner_id === user.id &&
-                <Label className="project-owner-label" variant="filled" color="green" isCompact>Owner</Label>
-          }
-        </DataListCell>
-      )));
-    }
-    return (
-      <React.Fragment>
-        <PageSection variant={PageSectionVariants.light}>
-          <Title headingLevel="h1" size='2xl' className="pf-v5-c-title">
-            <React.Fragment>
-              <span> Profile </span>
-              {user && user.is_superadmin &&
-                <Label className="super-admin-label" variant="filled" color="blue">Administrator</Label>
-              }
-            </React.Fragment>
-          </Title>
-        </PageSection>
-        <PageSection>
-          {!user && <Alert variant="danger" title="Error fetching user details" />}
-          {user &&
-          <DataList selectedDataListItemId={null} aria-label="User profile">
-            <DataListItem aria-labelledby="Name">
-              <DataListItemRow>
-                {!this.state.isEditing &&
-                  <DataListItemCells
-                    dataListCells={[
-                      <DataListCell key={1} width={2}><strong>Name:</strong></DataListCell>,
-                      <DataListCell key={2} width={4}>{user.name} <Button variant="link" icon={<PencilAltIcon />} onClick={this.onEditButtonClicked} isInline size="sm" ouiaId="edit-profile-button">Edit</Button></DataListCell>
-                    ]}
-                  />
-                }
-                {this.state.isEditing &&
-                  <DataListItemCells
-                    dataListCells={[
-                      <DataListCell key={1} width={2}><strong>Name:</strong></DataListCell>,
-                      <DataListCell key={2} width={4}>
-                        <InputGroup>
-                          <InputGroupItem isFill ><TextInput value={this.state.tempName} type="text" onChange={(_event, value) => this.setState({tempName: value})} aria-label="User name" /></InputGroupItem>
-                          <InputGroupItem>
-                            <Button variant="control" icon={<CheckIcon />} onClick={this.onSaveButtonClicked} ouiaId="edit-save-button">
-                              Save
-                            </Button>
-                          </InputGroupItem>
-                          <InputGroupItem>
-                            <Button variant="control" icon={<TimesIcon />} onClick={this.onCancelButtonClicked} ouiaId="edit-cancel-button">
-                              Cancel
-                            </Button>
-                          </InputGroupItem>
-                        </InputGroup>
-                      </DataListCell>
-                    ]}
-                  />
-                }
-              </DataListItemRow>
-            </DataListItem>
-            <DataListItem aria-labelledby="E-mail">
-              <DataListItemRow>
+  return (
+    <React.Fragment>
+      <PageSection variant={PageSectionVariants.light}>
+        <Title headingLevel="h1" size='2xl'>
+          <React.Fragment>
+            <span> Profile </span>
+            {user && user.is_superadmin &&
+              <Label className="super-admin-label" variant="filled" color="blue">Administrator</Label>
+            }
+          </React.Fragment>
+        </Title>
+      </PageSection>
+      <PageSection>
+        {!user && <Alert variant="danger" title="Error fetching user details" />}
+        {user &&
+        <DataList selectedDataListItemId={null} aria-label="User profile">
+          <DataListItem aria-labelledby="Name">
+            <DataListItemRow>
+              {!isEditing &&
                 <DataListItemCells
                   dataListCells={[
-                    <DataListCell key={1} width={2}><strong>E-mail:</strong></DataListCell>,
-                    <DataListCell key={2} width={4}>{user.email}</DataListCell>
+                    <DataListCell key={1} width={2}><strong>Name:</strong></DataListCell>,
+                    <DataListCell key={2} width={4}>{user.name} <Button variant="link" icon={<PencilAltIcon />} onClick={() => {
+                      setTempName(user.name);
+                      setIsEditing(true);
+                    }} isInline size="sm" ouiaId="edit-profile-button">Edit</Button></DataListCell>
                   ]}
                 />
-              </DataListItemRow>
-            </DataListItem>
-            <DataListItem aria-labelledby="Projects">
-              <DataListItemRow>
+              }
+              {isEditing &&
                 <DataListItemCells
                   dataListCells={[
-                    <DataListCell key={1} width={2}><strong>My Projects:</strong></DataListCell>,
-                    <DataListCell key={2} width={4} style={{paddingTop: 0, paddingBottom: 0}}>
-                      <DataList aria-label="projects" style={{borderTop: 'none'}}>
-                        {projects &&
-                          projectInfo
-                        }
-                      </DataList>
+                    <DataListCell key={1} width={2}><strong>Name:</strong></DataListCell>,
+                    <DataListCell key={2} width={4}>
+                      <InputGroup>
+                        <InputGroupItem isFill ><TextInput value={tempName} type="text" onChange={(_event, value) => setTempName(value)} aria-label="User name" /></InputGroupItem>
+                        <InputGroupItem>
+                          <Button variant="control" icon={<CheckIcon />} onClick={onSaveButtonClicked} ouiaId="edit-save-button">
+                            Save
+                          </Button>
+                        </InputGroupItem>
+                        <InputGroupItem>
+                          <Button variant="control" icon={<TimesIcon />} onClick={() => setIsEditing(false)} ouiaId="edit-cancel-button">
+                            Cancel
+                          </Button>
+                        </InputGroupItem>
+                      </InputGroup>
                     </DataListCell>
                   ]}
                 />
-              </DataListItemRow>
-            </DataListItem>
-          </DataList>
-          }
-        </PageSection>
-      </React.Fragment>
-    );
-  }
-}
+              }
+            </DataListItemRow>
+          </DataListItem>
+          <DataListItem aria-labelledby="E-mail">
+            <DataListItemRow>
+              <DataListItemCells
+                dataListCells={[
+                  <DataListCell key={1} width={2}><strong>E-mail:</strong></DataListCell>,
+                  <DataListCell key={2} width={4}>{user.email}</DataListCell>
+                ]}
+              />
+            </DataListItemRow>
+          </DataListItem>
+          <DataListItem aria-labelledby="Projects">
+            <DataListItemRow>
+              <DataListItemCells
+                dataListCells={[
+                  <DataListCell key={1} width={2}><strong>My Projects:</strong></DataListCell>,
+                  <DataListCell key={2} width={4} style={{paddingTop: 0, paddingBottom: 0}}>
+                    <DataList aria-label="projects" style={{borderTop: 'none'}}>
+                      {projects &&
+                        projects.map((project) => (
+                          <DataListCell key={project.name} className="pf-u-p-sm">
+                            <span> {project.title} </span>
+                            {project.owner_id === user.id &&
+                                  <Label className="project-owner-label" variant="filled" color="green" isCompact>Owner</Label>
+                            }
+                          </DataListCell>
+                        ))
+                      }
+                    </DataList>
+                  </DataListCell>
+                ]}
+              />
+            </DataListItemRow>
+          </DataListItem>
+        </DataList>
+        }
+      </PageSection>
+    </React.Fragment>
+  );
+};
+
+UserProfile.propTypes = {};
+
+export default UserProfile;
