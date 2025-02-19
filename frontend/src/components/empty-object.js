daf59838ad4ff7e1e2c7689544e08be1df2468cf
Mike Shriver <mshriver@redhat.com>
2025-02-19 08:46:29 -0500

    Convert EmptyObject (#576)

--- frontend/src/components/empty-object.js
+++ frontend/src/components/empty-object.js
@@ -16,33 +16,41 @@ import {
 
 import { NavLink } from 'react-router-dom';
 
-export class EmptyObject extends React.Component {
-  static propTypes = {
-    headingText: PropTypes.string,
-    bodyText: PropTypes.string,
-    returnLink: PropTypes.string,
-    returnLinkText: PropTypes.string
-  };
-  render () {
-    return (
-      <React.Fragment>
-        <EmptyState>
-          <EmptyStateHeader icon={<EmptyStateIcon icon={SearchIcon} />} />
-          <Text component="h1" size="lg">
-            {this.props.headingText ? this.props.headingText : 'This object couldn\'t be found.'}
-          </Text>
-          <EmptyStateBody>
-            {this.props.bodyText ? this.props.bodyText : 'Either the object doesn\'t exist or the ID is invalid.'}
-          </EmptyStateBody>
-          <EmptyStateFooter>
-            <NavLink style={{ color: 'white' }} to={!this.props.returnLink ?  '' : this.props.returnLink}>
-              <Button variant="primary" style = {{ margin: '25px' }}>
-                {this.props.returnLinkText ? this.props.returnLinkText : 'Return to dashboard'}
-              </Button>
-            </NavLink>
-          </EmptyStateFooter>
-        </EmptyState>
-      </React.Fragment>
-    );
-  }
-}
+const EmptyObject = (props) => {
+  const {
+    headingText,
+    bodyText,
+    returnLink,
+    returnLinkText
+  } = props;
+
+  return (
+    <React.Fragment>
+      <EmptyState>
+        <EmptyStateHeader icon={<EmptyStateIcon icon={SearchIcon} />} />
+        <Text component="h1" size="lg">
+          {headingText ? headingText : 'This object couldn\'t be found.'}
+        </Text>
+        <EmptyStateBody>
+          {bodyText ? bodyText : 'Either the object doesn\'t exist or the ID is invalid.'}
+        </EmptyStateBody>
+        <EmptyStateFooter>
+          <NavLink style={{ color: 'white' }} to={!returnLink ?  '' : returnLink}>
+            <Button variant="primary" style = {{ margin: '25px' }}>
+              {returnLinkText ? returnLinkText : 'Return to dashboard'}
+            </Button>
+          </NavLink>
+        </EmptyStateFooter>
+      </EmptyState>
+    </React.Fragment>
+  );
+};
+
+EmptyObject.propTypes = {
+  headingText: PropTypes.string,
+  bodyText: PropTypes.string,
+  returnLink: PropTypes.string,
+  returnLinkText: PropTypes.string
+};
+
+export default EmptyObject;
