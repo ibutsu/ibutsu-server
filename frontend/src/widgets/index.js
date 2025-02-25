0216351d59b854129a393ec7a7a41bcbfeb44e29
Diego Noceda <dnoceda@redhat.com>
2025-02-25 14:28:20 +0100

    Convert ResultAggregator widget (#572)
    * Convert ResultAggregator widget
    
    * get rid of callback
    
    * Use explicit props to configure the filter state of the widget

--- frontend/src/widgets/index.js
+++ frontend/src/widgets/index.js
@@ -2,4 +2,3 @@ export { ResultSummaryWidget } from './resultsummary';
 export { GenericAreaWidget } from './genericarea';
 export { GenericBarWidget } from './genericbar';
 export { FilterHeatmapWidget } from './filterheatmap';
-export { ResultAggregatorWidget } from './resultaggregator';
