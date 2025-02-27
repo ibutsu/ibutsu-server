05763ddf98fb58ba673cb177481ba1143b38fe6d
Will Cater <wcater@redhat.com>
2025-02-27 10:57:49 -0500

    Added logic for different keycloak auth paths (#567)
    * Added logic for different keycloak auth paths
    
    * Modified code with suggestions
    
    * Fixed linting issues
    
    Hardcoded Keycloak paths
    
    Removed /auth from server_url
    
    Linting changes
    
    Hardcoded Keycloak paths (#577)
    
    Added debug for frontend login
    
    Added logic for different keycloak auth paths (#567)
    
    * Added logic for different keycloak auth paths
    
    * Modified code with suggestions
    
    * Fixed linting issues
    
    Hardcoded Keycloak paths (#577)
    
    Isolated deploy main (#579)
    
    * Hardcoded Keycloak paths
    
    * Removed /auth from server_url
    
    * Linting changes
    
    Isolated deploy main (#580)
    
    * Hardcoded Keycloak paths
    
    * Removed /auth from server_url
    
    * Linting changes
    
    * Debug statements
    
    * Debug statements
    
    * linting
    
    Isolated deploy main (#583)
    
    * Hardcoded Keycloak paths
    
    * Removed /auth from server_url
    
    * Linting changes
    
    Added logic for different keycloak auth paths (#567) (#584)
    
    * Added logic for different keycloak auth paths
    
    * Modified code with suggestions
    
    * Fixed linting issues
    
    Hardcoded Keycloak paths
    
    Removed /auth from server_url
    
    Linting changes
    
    Hardcoded Keycloak paths (#577)
    
    Added debug for frontend login
    
    Reverted internal login path

--- backend/ibutsu_server/util/keycloak.py
+++ backend/ibutsu_server/util/keycloak.py
@@ -27,8 +27,7 @@ def get_keycloak_config(is_private=False):
         "authorization_url": build_url(realm_base_url, "protocol/openid-connect/auth"),
         "realm": realm,
         "client_id": current_app.config.get("KEYCLOAK_CLIENT_ID"),
-        "redirect_uri": backend_url
-        + current_app.config.get("KEYCLOAK_AUTH_PATH", "/login/auth/keycloak"),
+        "redirect_uri": backend_url + "/login/auth/keycloak",
     }
     print(f"authorization_url: {config['authorization_url']} ")
     if current_app.config.get("KEYCLOAK_ICON"):
