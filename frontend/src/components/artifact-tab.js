cd23eb9519d4947e52acc351cb86d48f3d451501
mshriver <mshriver@redhat.com>
2025-03-14 12:08:24 -0400

    Use async/await and memos in ArtifactTab

--- frontend/src/components/artifact-tab.js
+++ frontend/src/components/artifact-tab.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardBody, CardFooter, Text } from '@patternfly/react-core';
 import { Editor } from '@monaco-editor/react';
@@ -6,71 +6,71 @@ import DownloadButton from './download-button';
 import { Settings } from '../settings';
 import { HttpClient } from '../services/http';
 
-
-const ArtifactTab = (props) => {
-  const {artifact, } = props;
-
+const ArtifactTab = ({ artifact }) => {
   const [blob, setBlob] = useState();
-  const [blobType, setBlobType ]= useState();
+  const [blobType, setBlobType] = useState();
 
-  useEffect(()=>{
-    HttpClient.get([Settings.serverUrl, 'artifact', artifact.id, 'view'])
-      .then(response => {
-        let contentType = response.headers.get('Content-Type');
-        if (contentType.includes('text')) {
-          response.text().then(text => {
-            setBlob(text);
-            setBlobType('text');
-          });
-        }
-        else if (contentType.includes('image')) {
-          response.blob().then(image => {
-            setBlob(image);
-            setBlobType('image');
-          });
-        } else {
-          console.log('bad news bears, artifact is neither image nor text.');
-        }
-      })
-      .catch((error) => console.error(error));
-  }, [artifact]);
+  const fetchArtifact = useCallback(async () => {
+    try {
+      const response = await HttpClient.get([Settings.serverUrl, 'artifact', artifact.id, 'view']);
+      const contentType = response.headers.get('Content-Type');
+      if (contentType.includes('text')) {
+        const text = await response.text();
+        setBlob(text);
+        setBlobType('text');
+      } else if (contentType.includes('image')) {
+        const image = await response.blob();
+        setBlob(image);
+        setBlobType('image');
+      } else {
+        console.log('bad news bears, artifact is neither image nor text.');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, [artifact.id]);
 
+  useEffect(() => {
+    fetchArtifact();
+  }, [fetchArtifact]);
 
+  const cardBody = useMemo(() => {
+    if (!blob) {
+      return <Text>Blob is loading</Text>;
+    }
+    if (blobType === 'text') {
+      return (
+        <Editor
+          key={artifact.id}
+          fontFamily="Noto Sans Mono, Hack, monospace"
+          theme="vs-dark"
+          value={blob}
+          height="40rem"
+          options={{ readOnly: true }}
+        />
+      );
+    } else if (blobType === 'image') {
+      return <img key={artifact.id} src={URL.createObjectURL(blob)} alt={artifact.filename} />;
+    }
+  }, [blob, blobType, artifact.id, artifact.filename]);
 
-  let cardBody = <Text>Blob is loading</Text>;
-  if (blobType === 'text') {
-    cardBody = <Editor
-      key={artifact.id}
-      fontFamily="Noto Sans Mono, Hack, monospace"
-      theme="vs-dark"
-      value={blob}
-      height="40rem"
-      options={{readOnly: true}}
-    />;
-  } else if (blobType === 'image') {
-    cardBody = <img
-      key={artifact.id}
-      src={URL.createObjectURL(blob)}
-      alt={artifact.filename}
-    />;
-  }
   return (
     <Card>
-      <CardBody>
-        {cardBody}
-      </CardBody>
+      <CardBody>{cardBody}</CardBody>
       <CardFooter>
         <DownloadButton
           url={`${Settings.serverUrl}/artifact/${artifact.id}/download`}
           filename={artifact.filename}
-        >Download {artifact.filename}</DownloadButton>
+        >
+          Download {artifact.filename}
+        </DownloadButton>
       </CardFooter>
     </Card>
   );
 };
 
 ArtifactTab.propTypes = {
-  artifact: PropTypes.object
+  artifact: PropTypes.object.isRequired,
 };
 
 export default ArtifactTab;
