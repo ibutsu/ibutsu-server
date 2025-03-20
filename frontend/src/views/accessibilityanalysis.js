3e5511ecc8c28c89df4c9cad6e46cd570ef2ef46
Mike Shriver <mshriver@redhat.com>
2025-03-20 12:59:59 -0400

    Replace react-json with patternfly codeeditor (#606)
    * Replace react-json with patternfly codeeditor
    
    * Update result with memos for artifact Tabs and object jSON

--- frontend/src/views/accessibilityanalysis.js
+++ frontend/src/views/accessibilityanalysis.js
@@ -28,42 +28,22 @@ import {
 import { Link, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { HttpClient } from '../services/http';
 import { Settings } from '../settings';
-import { JSONTree } from 'react-json-tree';
 import {
   resultToRow,
 } from '../utilities';
 import FilterTable from '../components/filtertable';
 import { IbutsuContext } from '../services/context';
 import TabTitle from '../components/tabs';
+import { CodeEditor, Language } from '@patternfly/react-code-editor';
 
 const COLUMNS = ['Test', 'Run', 'Result', 'Duration', 'Started'];
-
-const JSONTHEME = {
-  scheme: 'monokai',
-  author: 'wimer hazenberg (http://www.monokai.nl)',
-  base00: '#272822',
-  base01: '#383830',
-  base02: '#49483e',
-  base03: '#75715e',
-  base04: '#a59f85',
-  base05: '#f8f8f2',
-  base06: '#f5f4f1',
-  base07: '#f9f8f5',
-  base08: '#f92672',
-  base09: '#fd971f',
-  base0A: '#f4bf75',
-  base0B: '#a6e22e',
-  base0C: '#a1efe4',
-  base0D: '#66d9ef',
-  base0E: '#ae81ff',
-  base0F: '#cc6633',
-};
 
 const AccessibilityAnalysisView = (props) => {
   const {
     view
   } = props;
   const context = useContext(IbutsuContext);
+  const {darkTheme} = context;
   const location = useLocation();
   const navigate = useNavigate();
   const params = useSearchParams();
@@ -350,8 +330,14 @@ const AccessibilityAnalysisView = (props) => {
           </Tab>
           <Tab eventKey="run-object" title={<TabTitle icon={<CodeIcon/>} text="Run Object" />} style={{backgroundColor: 'white'}}>
             <Card>
-              <CardBody>
-                <JSONTree data={run} theme={JSONTHEME} invertTheme hideRoot shouldExpandNodeInitially={() => true}/>
+              <CardBody id='object-card-body'>
+                <CodeEditor
+                  isReadOnly={true}
+                  isDarkTheme={darkTheme}
+                  language={Language.json}
+                  code={JSON.stringify(run, null, '\t')}
+                  height="sizeToFit"
+                />
               </CardBody>
             </Card>
           </Tab>
