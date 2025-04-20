92ce86f043db03cb3ed2427c45d66aff8c3c0437
mshriver <mshriver@redhat.com>
2025-04-20 13:07:57 -0400

    Somehow this is making the rule disable work?

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,5 +1,4 @@
 // eslint.config.mjs
-import cypress from 'eslint-plugin-cypress';
 import reactPlugin from 'eslint-plugin-react';
 import unusedImports from 'eslint-plugin-unused-imports';
 import globals from 'globals';
@@ -7,30 +6,25 @@ import babelParser from '@babel/eslint-parser';
 import {defineConfig} from 'eslint/config';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
-});
+import pluginCypress from 'eslint-plugin-cypress/flat';
 
 export default defineConfig([
-  ...compat.config(reactHooksPlugin.configs.recommended),
-  ...compat.config(reactPlugin.configs.recommended),
-  ...compat.config(js.configs.recommended),
-  ...compat.config(cypress.configs.recommended),
-  // ...compat.config(prettier.configs.recommended),
   {
     files: ['src/*', 'cypress/*', 'bin/*'],
     ignores: ['node_modules/*', 'build/*'],
   },
+  pluginCypress.configs.recommended,
+  reactHooksPlugin.configs['recommended-latest'],
+  reactPlugin.configs.flat.recommended,
+  js.configs.recommended,
+  pluginCypress.configs.recommended,
+  // prettier.configs.recommended
   {
     plugins: {
-      js,
-      reactPlugin,
-      reactHooksPlugin,
-      cypress,
-      'unused-imports': unusedImports
+      'unused-imports': unusedImports, // not flat config compatible
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
     },
     languageOptions: {
       globals: {
@@ -63,13 +57,6 @@ export default defineConfig([
       },
     },
     rules: {
-      'cypress/no-assigning-return-values': 'error',
-      'cypress/no-unnecessary-waiting': 'error',
-      'cypress/assertion-before-screenshot': 'warn',
-      'cypress/no-force': 'warn',
-      'cypress/no-async-tests': 'error',
-      'cypress/no-async-before': 'error',
-      'cypress/no-pause': 'error',
       'react/jsx-curly-brace-presence': [
         'error',
         {
@@ -77,15 +64,13 @@ export default defineConfig([
           children: 'never',
         },
       ],
-
-      'arrow-body-style': ['error', 'as-needed'],
       'react/react-in-jsx-scope': 'off',
+      'arrow-body-style': ['error', 'as-needed'],
       camelcase: 'off',
       'spaced-comment': 'error',
       quotes: ['warn', 'single'],
       'no-duplicate-imports': 'error',
       'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': ['warn'],
       'semi': ['error'],
       'indent': ['error', 2],
       'space-before-function-paren': ['error', 'always'],
